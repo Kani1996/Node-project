9b3f7d1eab29af86f7d12d5c337918f43c6ea324
kanimozhi vivekanandan <34624127+Kani1996@users.noreply.github.com>
2020-01-08 16:16:15 +0530

    Usage of http and OS libraries

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
-var server = app.listen(3000, function(){
-    console.log("Application is listening at port 3000");
-})
-
-app.get('/',function(req,res){
-    res.send('Hello world');
-});
+var http = require("http");
+const os=require('os');
+http.createServer(function(request,response){
+    response.writeHead(200,{'content-type':'text/plain'});
+    //response.end("Hello world!!!");
+    setInterval(function(){
+        console.log("setInterval: Hey! 1 millisecond completed!..");   
+        response.end("Finding the free memory using OS library in node:"+os.freemem());
+    },5000);
+    setTimeout(function(){
+       console.log("Data printed after timeout");
+    },1000)
+}).listen(3000);
